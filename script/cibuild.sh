d25f6a6cdbe2dd5a430b388738cd7136dc365633
jstruzik <jake@robinpowered.com>
2016-04-05 22:06:47 -0400

    Fixing feed link, updating htmlproofer config

--- script/cibuild.sh
+++ script/cibuild.sh
@@ -2,5 +2,5 @@
 set -e # halt script on error
 
 bundle exec jekyll build
-bundle exec htmlproofer ./_site --disable-external
+bundle exec htmlproofer ./_site --disable-external --empty-alt-ignore
 bundle exec s3_website push
